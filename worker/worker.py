683bad0e3f8d24b34168c1cc5f85a63f4dcd0ef3
Guillermo Celarie <lg.celarie@gmail.com>
2024-02-03 22:18:02 -0600

    usage of shutil to move files between folders

--- worker/worker.py
+++ worker/worker.py
@@ -1,7 +1,9 @@
 import pika, sys, os, subprocess
+import shutil
 import functools
 import logging
 import requests
+from pathlib import Path
 from pika.exchange_type import ExchangeType
 
 LOG_FORMAT = ('%(levelname) -10s %(asctime)s %(name) -30s %(funcName) '
@@ -12,26 +14,50 @@ logging.basicConfig(level=logging.INFO, format=LOG_FORMAT)
 
 def on_message(chan, method_frame, header_frame, body, userdata=None):
     src_folder='/origin'
+    subfolders = ['series','movies']
     dst_folder='/destination'
+    plex_host = os.getenv('PLEX_HOST')
     """Called when a message is received. Log message and ack it."""
     LOGGER.info('Delivery properties: %s, message metadata: %s', method_frame, header_frame)
     LOGGER.info('Userdata: %s, message body: %s', userdata, body)
     if body.decode() == 'sync':
-            print('Recieved sync message. Processing...')
-            subprocess.run(f'mv {src_folder} {dst_folder}/', shell=True)
-            print('Moving process completed.')
-            print('Sending plex the request to sync library')
+            LOGGER.info('Recieved sync message. Processing...')
+            for subfolder in subfolders:
+                move_files( Path(src_folder).joinpath(subfolder), Path(dst_folder).joinpath(subfolder))
+            LOGGER.info('Moving process completed.')
+            LOGGER.info('Sending plex the request to sync library')
             params = {'X-Plex-Token': os.getenv('PLEX_TOKEN')}
             try:
-                response = requests.get('192.168.1.152:32400/library/sections/29/refresh', params=params)
-                print('Request to plex server sent... mission complete!')
+                response = requests.get(f'http://{plex_host}/library/sections/29/refresh', params=params)
+                LOGGER.info('Request to plex server sent... mission complete!')
             except requests.exceptions.ConnectionError:
-                print('A connection error ocurred')
+                LOGGER.error('A connection error ocurred')
             except requests.exceptions.HTTPError:
-                print(f'a {response.status_code} was returned')
+                LOGGER.error(f'a {response.status_code} was returned')
     else:
-        print('unknown message received')
+        LOGGER.warning('unknown message received')
     chan.basic_ack(delivery_tag=method_frame.delivery_tag)
+
+def move_files(source, destination):
+    if not os.path.exists(source):
+        LOGGER.error(f"Source directory '{source}' does not exist.")
+        return
+
+    if not os.path.exists(destination):
+        os.makedirs(destination)
+
+    # Iterate through all items in the source directory
+    for item in os.listdir(source):
+        source_path = os.path.join(source, item)
+        destination_path = os.path.join(destination, item)
+
+        # If it's a directory, recursively copy it
+        if os.path.isdir(source_path):
+            move_files(source_path, destination_path)
+        else:
+            # If it's a file, copy it
+            shutil.move(source_path, destination_path)
+            LOGGER.info(f"Moved '{item}' to '{destination}'")
 
 def main():
     """Main method."""
